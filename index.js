9e0cd35918bf81bc946aecc7bb89990bd5f05ba4
Jesus Orozco <jesusorozco@jesuss-mbp.myfiosgateway.com>
2021-12-07 21:23:58 -0500

    part 2

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const { Server, request } = require('http')
 const teams = require('./teams')
 
 const app = express()
+const bodyParser = require('body-parser')
+
+
+const checkTeam = (request, response, next) => {
+
+
+}
+
+
 
 app.get('/teams', (request, response) => {
   return response.send(teams)
@@ -16,6 +25,25 @@ app.get('/teams/:id', (request, response) => {
   return response.send(foundTeam)
 })
 
+app.post('/', bodyParser.json(), (request, response) => {
+  const {
+    id, location, mascot, abbreviation, conference, division
+  } = request.body
+
+  if (!id || !location || !mascot || !abbreviation || !conference || !division) {
+    return response.status(400).send('This is an error 400')
+  }
+  const newTeam = {
+    id, location, mascot, abbreviation, conference, division
+  }
+
+  teams.push(newTeam)
+
+  return response.status(201).send(newTeam)
+})
+
+app.put('/',)
+// This has to be last.
 app.all('*', (request, response) => {
   return response.status(404).send('This is a 404 error')
 })
